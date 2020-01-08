c200e97280dd0f445bf3ad781ea9e8492d75278e
Mauricio <31927735+villaltam@users.noreply.github.com>
2020-01-08 16:16:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', ()=> {
+    let long;
+    let lat;
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            cons api = 'https://api.darksky.net/forecast/6325e4049a204a500dc19fe5da49aba0/${lat},${long}'
+        });
+        fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const {temperature, summary} = data.currently;
+            })
+    }else{
+        h1.tectContemt = "Please allow your browswer to provide your location"
+    }
+});
